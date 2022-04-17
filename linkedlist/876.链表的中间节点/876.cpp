b4d4c5427c9637176fec7d0b54a4e48e78e7e397
Y <1913145200@qq.com>
2022-04-17 20:40:46 +0800

    第876题

---
+++ linkedlist/876.链表的中间节点/876.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+    
+};
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        int n = 0;
+        ListNode* cur = head;
+        while (cur != nullptr) {
+            ++n;
+            cur = cur->next;
+        }
+        int k = 0;
+        cur = head;
+        while (k < n / 2) {
+            ++k;
+            cur = cur->next;
+        }
+        return cur;
+    }
+};
