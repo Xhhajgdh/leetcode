f375c8f3bab42cfa097b9c36ae6afb0be1ba5b96
Y <1913145200@qq.com>
2022-05-19 21:29:46 +0800

    lc设计链表

---
+++ linkedlist/LC设计链表/lc设计链表.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+using namespace std;
+class MyLinkedList {
+private:
+    struct node {
+        int val;
+        node* pre;
+        node* next;
+        node(int _val) {
+            val = _val;
+            pre = NULL;
+            next = NULL;
+        }
+    };
+    int size;
+    node* head;
+public:
+    MyLinkedList() {
+        size = 0;
+        head = new node(0);
+    }
+
+    int get(int index) {
+        if (index < 0 || index >= size)
+            return -1;
+        node* cur = head->next;
+        while (index--)
+            cur = cur->next;
+        return cur->val;
+    }
+
+    void addAtHead(int val) {
+        node* newnode = new node(val);
+        if (head->next)
+            head->next->pre = newnode;
+        newnode->next = head->next;
+        newnode->pre = head;
+        head->next = newnode;
+        size++;
+    }
+
+    void addAtTail(int val) {
+        node* cur = head;
+        node* newnode = new node(val);
+        while (cur->next)
+            cur = cur->next;
+        cur->next = newnode;
+        newnode->pre = cur;
+        newnode->next = NULL;
+        size++;
+    }
+
+    void addAtIndex(int index, int val) {
+        if (index <= 0)
+            addAtHead(val);
+        else if (index == size)
+            addAtTail(val);
+        else if (index > size)
+            return;
+        else {
+            node* cur = head->next;
+            node* newnode = new node(val);
+            while (index--)
+                cur = cur->next;
+            newnode->next = cur;
+            newnode->pre = cur->pre;
+            cur->pre->next = newnode;
+            cur->pre = newnode;
+            size++;
+        }
+    }
+
+    void deleteAtIndex(int index) {
+        if (index < 0 || index >= size)
+            return;
+        node* cur = head->next;
+        while (index--)
+            cur = cur->next;
+        if (cur->next) {
+            cur->next->pre = cur->pre;
+            cur->pre->next = cur->next;
+        }
+        else {
+            cur->pre->next = NULL;
+        }
+        delete cur;
+        size--;
+    }
+};
