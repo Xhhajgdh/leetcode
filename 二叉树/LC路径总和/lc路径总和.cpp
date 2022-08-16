f6aa53e75c56e1d01cd2dfce0625a82237fff72e
Y <1913145200@qq.com>
2022-08-16 17:57:13 +0800

    lc路径总和

---
+++ 二叉树/LC路径总和/lc路径总和.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+    
+};
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if (root == nullptr) {
+            return false;
+        }
+        queue<TreeNode*> que_node;
+        queue<int> que_val;
+        que_node.push(root);
+        que_val.push(root->val);
+        while (!que_node.empty()) {
+            TreeNode* now = que_node.front();
+            int temp = que_val.front();
+            que_node.pop();
+            que_val.pop();
+            if (now->left == nullptr && now->right == nullptr) {
+                if (temp == sum) {
+                    return true;
+                }
+                continue;
+            }
+            if (now->left != nullptr) {
+                que_node.push(now->left);
+                que_val.push(now->left->val + temp);
+            }
+            if (now->right != nullptr) {
+                que_node.push(now->right);
+                que_val.push(now->right->val + temp);
+            }
+        }
+        return false;
+    }
+};
